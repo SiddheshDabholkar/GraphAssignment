d60b17a47d688e66e4763b14ccab0b5ee6716403
Siddhesh Dabholkar <iamsiddhesh22@gmail.com>
2022-04-10 06:45:43 +0530

    header

--- src/Layout/Home/Header.tsx
+++ src/Layout/Home/Header.tsx
@@ -1,8 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Header.scss";
 
 const Header: React.FC = () => {
-  return <div className="Header">Header</div>;
+  return (
+    <div className="Header">
+      <div className="HeaderLeft">
+        <Link to="/" className="Link">
+          Schedules
+        </Link>
+      </div>
+      <div className="HeaderRight">
+        <Link to="/" className="Link">
+          Home
+        </Link>
+        <Link to="/bonus" className="Link">
+          Bonus
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- src/index.tsx
+++ src/index.tsx
@@ -7,11 +7,11 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <HomeLayout>
-      <BrowserRouter>
+    <BrowserRouter>
+      <HomeLayout>
         <App />
-      </BrowserRouter>
-    </HomeLayout>
+      </HomeLayout>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
